83ad46006a3652a98f00d8b6fb06941a7a5842b5
Jakub Stas <jakub@jakubstas.com>
2016-04-10 16:03:07 +0100

    Initial commit

---
+++ src/main/java/com/jakubstas/ignis/IgnisApplication.java
@@ -0,0 +1,12 @@
+package com.jakubstas.ignis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgnisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgnisApplication.class, args);
+	}
+}
